4052487aea8b30240db16d40a65933cf0f66c3aa
papabn <pndiaye999@gmail.com>
2025-08-10 01:09:31 +0000

    style themee with shadcn

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -3,16 +3,26 @@ import React from 'react'
 
 const MainNav = () => {
   return (
-    <div className="">
-      <Link href="/" className="text-2xl font-bold">
-        dashboard
-      </Link>
-      <Link href="/tickets" className="text-2xl font-bold">
-        Tickets
-      </Link>
-      <Link href="/users" className="text-2xl font-bold">
-        Users
-      </Link>
+    <div className="fleex justify-between">
+      <div className="">
+        <Link href="/" className="text-2xl font-bold">
+          dashboard
+        </Link>
+        <Link href="/tickets" className="text-2xl font-bold">
+          Tickets
+        </Link>
+        <Link href="/users" className="text-2xl font-bold">
+          Users
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/" className="text-2xl font-bold">
+            Logout
+          </Link>
+          <Link href="/auth/register" className="text-2xl font-bold">
+            Dark
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
