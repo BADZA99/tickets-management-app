42fc29fbf3abceea48b46c269bd7bbbf25b52a68
papabn <pndiaye999@gmail.com>
2025-08-13 03:08:44 +0000

    server pagination

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -5,25 +5,45 @@ import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 import Pagination from '@/components/Pagination';
 
-const Tickets =async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParams{
+  page: string;
+}
+
+
+const Tickets = async ({ searchParams }: { searchParams :SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
   // console.log(tickets);
+  const tickets = await prisma.ticket.findMany({
+    
+    skip: (page - 1) * pageSize,
+    take: pageSize,
+    // orderBy: {
+    //   createdAt: 'desc',
+    // },
+  });
+
   return (
     <div>
-      <Link href='/tickets/new'
-      className={buttonVariants({
-        variant: 'default'})}
-      >new ticket</Link>
-      
+      <Link
+        href="/tickets/new"
+        className={buttonVariants({
+          variant: "default",
+        })}
+      >
+        new ticket
+      </Link>
+
       <DataTable tickets={tickets} />
-      <Pagination 
-        itemCount={22}
-        pageSize={10}
-        currentPage={1}
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
         // onPageChange={(page) => console.log('Page changed to:', page)}
-       />
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Tickets
