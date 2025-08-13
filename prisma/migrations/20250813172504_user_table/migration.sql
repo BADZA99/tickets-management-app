afe89524a8244e96b6025bc04708ac6c8c8254e7
papabn <pndiaye999@gmail.com>
2025-08-13 17:26:13 +0000

    user table and its relation with tickets

---
+++ prisma/migrations/20250813172504_user_table/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE `ticket` ADD COLUMN `assignedToUserId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
