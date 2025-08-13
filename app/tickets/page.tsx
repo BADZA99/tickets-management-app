921c01e827f20ea64fc51ad5566de0ab744040eb
papabn <pndiaye999@gmail.com>
2025-08-13 16:49:21 +0000

    status filter prisma and fix bug pagination based on filter

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,32 +1,57 @@
-import React from 'react'
-import {prisma} from "../../prisma/prisma";
-import DataTable from './DataTable';
-import Link from 'next/link';
-import { buttonVariants } from '@/components/ui/button';
-import Pagination from '@/components/Pagination';
-import StatusFilter from '@/components/StatusFilter';
+import React from "react";
+import { prisma } from "../../prisma/prisma";
+import DataTable from "./DataTable";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status } from "@prisma/client";
 
-interface SearchParams{
+interface SearchParams {
+  status: Status;
   page: string;
 }
 
-
-const Tickets = async ({ searchParams }: { searchParams :SearchParams }) => {
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
   const page = parseInt(searchParams.page) || 1;
-  const ticketCount = await prisma.ticket.count();
+
+  const statues = Object.values(Status);
+  const status = statues.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let whereClause = {};
+
+  if (status) {
+    whereClause = { status };
+  } else {
+    whereClause = {
+      NOT: [
+        {
+          status: "CLOSED" as Status,
+        },
+      ],
+    };
+  }
   // console.log(tickets);
+  // Compte le nombre total de tickets en fonction du filtre de statut
+  const ticketCount = await prisma.ticket.count({
+    where: whereClause,
+  });
   const tickets = await prisma.ticket.findMany({
-    
+    where: whereClause,
     skip: (page - 1) * pageSize,
     take: pageSize,
     // orderBy: {
-    //   createdAt: 'desc',
+    //   createdAt: "desc",
     // },
   });
 
   return (
     <div>
+      <div className="flex gap-2">
+
       <Link
         href="/tickets/new"
         className={buttonVariants({
@@ -35,8 +60,9 @@ const Tickets = async ({ searchParams }: { searchParams :SearchParams }) => {
       >
         new ticket
       </Link>
-      
+
       <StatusFilter />
+      </div>
 
       <DataTable tickets={tickets} />
       <Pagination
@@ -49,4 +75,4 @@ const Tickets = async ({ searchParams }: { searchParams :SearchParams }) => {
   );
 };
 
-export default Tickets
+export default Tickets;
