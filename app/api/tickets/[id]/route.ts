cd3e351fe064bbf875506b46de8a945952117893
papabn <pndiaye999@gmail.com>
2025-08-13 00:33:37 +0000

    update ticket api

---
+++ app/api/tickets/[id]/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/prisma/prisma";
+import { ticketSchema } from "@/ValidationSchemas/ticket";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props{
+    params:{id:string}
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+    const body = await request.json();
+    const validation = ticketSchema.safeParse(body);
+    
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), {
+            status: 400,
+        });
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+        where: { id: parseInt(params.id) },
+    });
+
+    if (!ticket) {
+        return NextResponse.json({ error: "Ticket not found" }, { status: 404 });   
+    }
+
+    const updatedTicket = await prisma.ticket.update({
+        //quand on recupere lid de la requete c en string alors que quand on le stocke dans la base de donnee c en int
+        //donc on parse le string en int
+        where: { id: parseInt(params.id) },
+        data: {
+            ...body
+        },
+    });
+
+    return NextResponse.json(updatedTicket, {
+        status: 200,
+    });
+}
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {prisma} from "../../prisma/prisma";
 import DataTable from './DataTable';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 
 const Tickets =async () => {
   const tickets = await prisma.ticket.findMany();
   // console.log(tickets);
   return (
     <div>
+      <Link href='/tickets/new'
+      className={buttonVariants({
+        variant: 'default'})}
+      >new ticket</Link>
       <DataTable tickets={tickets} />
     </div>
   )
