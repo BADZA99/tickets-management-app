148cf28682ccdb37d89d6a1a754c5d557de6c273
papabn <pndiaye999@gmail.com>
2025-08-15 21:27:48 +0000

    dashboard paGE SETUP

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
+import DashChart from '@/components/DashChart';
+import DashRecentTickets from '@/components/DashRecentTickets';
+import { prisma } from '@/prisma/prisma';
+import { skip } from 'node:test';
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = async() => {
+  // Fetching the latest 5 tickets that are not closed
+  // and including the assigned user details
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT:[
+        {status: 'CLOSED'},
+      ]
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    skip: 0,
+    take: 5,
+    include : {
+      assignedTo: true
+  }
+  });
+
+  // console.log(tickets);
+
+  // Grouping tickets by status and counting them
+  const groupTicket=await prisma.ticket.groupBy({
+    by: ['status'],
+    _count: {
+      id: true,
+    },
+  })
+  console.log(groupTicket);
+
   return (
     <div>
-       Dashboard 
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div><DashRecentTickets/></div>
+        <div><DashChart/></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard
